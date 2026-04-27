24a35800be20e27d65aa684b3dcf1f0e43965b77
Ryan Markowitz <ryanmarkowitzmedia@gmail.com>
2026-04-27 18:14:16 -0400

    a

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <nimble_init.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 void app_main(void)
 {
     // Initialize nimBLE
+    vTaskDelay(pdMS_TO_TICKS(3000)); // 3 second delay
     nimble_init();
 }
